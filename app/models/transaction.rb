72d2b2626965d1e6980335b2f289409fa19156ae
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-14 19:01:20 -0300

    Add model transaction with validation and association.

---
+++ app/models/transaction.rb
@@ -0,0 +1,8 @@
+class Transaction < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :category_id, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 1}
+
+  belongs_to :category
+  belongs_to :user
+end
