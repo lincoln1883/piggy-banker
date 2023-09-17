b15f03e975826ce069ec126b64a470a707029df3
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-17 13:30:11 -0300

    Add adjustment to the authorization.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,15 +1,11 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
   def initialize(user)
-    can :read, Category
     return unless user.present?
 
     can :read, :all
-
-    can :manage, Category, author_id: user.id
-    can :manage, Transaction, author_id: user.id
+    can :manage, Category, user_id: user.id
+    can :manage, Transaction, user_id: user.id
 
     return unless user.admin?
 
