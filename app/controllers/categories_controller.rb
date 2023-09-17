696d8f2699dff26fe03e3cecf2cd4947167ce3fa
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-17 13:09:31 -0300

    Add category controller and actions.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,32 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+  load_and_authorize_resource
+  def index
+    @categories = Category.includes(:user, :transactions).all
+  end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = current_user.categories.build(category_params)
+    if @category.save
+      redirect_to categories_path, notice: 'Category created successfully!'
+    else
+      flash[:alert] = 'Category could not be saved validation errors'
+      render :new
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon, :author_id)
+  end
 end
