68e36fcf04c959e9f566668d57a1390e2f527207
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-17 15:28:14 -0300

    Add factory setup for user, category, transaction.

---
+++ spec/factories/transactions.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :transaction do
+    sequence(:name) { |n| "Transaction #{n}" }
+    amount { Faker::Number.decimal(l_digits: 2) }
+    category
+    user
+  end
+end
